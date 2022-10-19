36299ce8cc521214ed0569c65279900ef3483e0f
Rai Alcides <railayli2@hotmail.com>
2022-10-19 17:52:53 -0300

    ajustes no js do via cep

--- exercicios/nome-raiani/projeto-casa/ViaCEP/script.js
+++ exercicios/nome-raiani/projeto-casa/ViaCEP/script.js
@@ -1,10 +1,23 @@
 
-
-
-async function pegarCep(cep){
-    const resposta = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
-    const endereco = await resposta.json()
-    console.log(endereco)
+function criarCards(endereco){
+    return `
+        <div class="card">
+            <p>${endereco.logradouro}</p>
+            <p>${endereco.bairro}</p>
+            <p>${endereco.localidade}</p>
+            <p>${endereco.ddd}</p>
+        </div>
+    `
 }
 
-pegarCep('41218011')
+
+async function pegarCep(){
+     let cep = document.getElementById('input').value
+     console.log(cep)
+
+    const resposta = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
+    const endereco = await resposta.json()
+    document.getElementById('main').innerHTML = criarCards(endereco)
+    console.log(endereco)
+
+}
