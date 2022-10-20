598ab4db3f4896842e1ccf487e07e758d7c8b96f
Rai Alcides <railayli2@hotmail.com>
2022-10-20 12:11:38 -0300

    exercicio via cep, finalizado

--- exercicios/nome-raiani/projeto-casa/ViaCEP/script.js
+++ exercicios/nome-raiani/projeto-casa/ViaCEP/script.js
@@ -1,24 +1,44 @@
 
+
+
+
+
 function criarCards(endereco){
     return `
         <div class="card">
-            <p>${endereco.logradouro}</p>
-            <p>${endereco.bairro}</p>
-            <p>${endereco.localidade}</p>
-            <p>${endereco.uf}</p>
-            <p>${endereco.ddd}</p>
+            <p> Logradouro: <br> ${endereco.logradouro}</p>
+            <hr>
+            <p> Bairro: <br> ${endereco.bairro}</p>
+            <hr>
+            <p> Localidade: <br>  ${endereco.localidade}</p>
+            <hr>
+            <p> Uf: <br> ${endereco.uf}</p>
+            <hr>
+            <p> DDD: <br> ${endereco.ddd}</p>
+            <hr>
         </div>
     `
 }
 
 
 async function pegarCep(){
-     let cep = document.getElementById('input').value
-     console.log(cep)
+    let input = document.getElementById('input')
+    let mensagemErro = document.querySelector('.mensagem-erro')
 
-    const resposta = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
-    const endereco = await resposta.json()
-    document.getElementById('main').innerHTML = criarCards(endereco)
-    console.log(endereco)
+    if (input.value === '') {
+        mensagemErro.style.display = 'block'
+    } else {
+        mensagemErro.style.display = 'none'
+
+        let cep = input.value
+        console.log(cep)
+   
+       const resposta = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
+       const endereco = await resposta.json()
+       document.getElementById('main').innerHTML = criarCards(endereco)
+       console.log(endereco)
+   
+       input.value = ""
+    }
 
 }
