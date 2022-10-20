4294ade692a6fd9f436de07a07a9e1dae661d722
Laylla Farias <layllandradef@live.com>
2022-10-20 12:19:49 -0300

    Exercicio Ghibli finalizado

--- exercicios/laylla-emilly/projeto-casa/Ghibli/script.js
+++ exercicios/laylla-emilly/projeto-casa/Ghibli/script.js
@@ -10,21 +10,15 @@ async function getFilmes() {
         `<div class="cards">
             <h1 class="titulo">${filme.title}</h1>
             <h2 class="titulo-original">${filme.original_title}</h2>
-            <h3 class="titulo-original">${filme.original_title_romanised}</h3>
+            <h3 class="titulo-dois">${filme.original_title_romanised}</h3>
             <img class="imagem" src=${filme.image}>
             <img class="banner" src=${filme.movie_banner}>
             <p class="descricao">${filme.description}</p>
-            <h4 class="diretor">${filme.director}</h4>
-            <h5 class="producao">${filme.producer}</h5>
-            <h5 class="data-lancamento>${filme.release_date}</h5>
-            <h6 class="tempo-execucao>${filme.running_time}</h6>
-            <h6 class="score">${filme.rt_score}</h6>
-            <p class="pessoas">${filme.people}</p>
-            <h6 class="especies">${filme.species}</h6
-            <p class="id">${filme.id}</p>
-            <h6 class="localizacao">${filme.locations}</h6>
-            <h6 class="veiculos">${filme.vehicles}</h6>
-            <h6 class="url">${filme.url}</h6>
+            <h4 class="diretor">Diretor: ${filme.director}</h4>
+            <h5 class="producao">Produção: ${filme.producer}</h5>
+            <h5 class="data-lancamento">Data de Lançamento: ${filme.release_date}</h5>
+            <h6 class="tempo-execucao">Tempo de Execução: ${filme.running_time}</h6>
+            <h6 class="score">Avaliação: ${filme.rt_score}</h6>
         </div>
         `
       })
